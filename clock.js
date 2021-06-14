f74acf85a13d7edff32871feb20b55368194f434
Trevor Mearns <spiral272@gmail.com>
2021-06-14 12:26:47 +0900

    ready for code review approval

--- clock.js
+++ clock.js
@@ -1,98 +1,87 @@
 'use strict';
 
-function showTimeAndDate() {
-  
-  let dateObj = new Date();
-  let y = '0';
+function showTime() {
+  const theTime = new Date();
+  const seconds = addLeadingZero(theTime.getSeconds());
+  const minutes = addLeadingZero(theTime.getMinutes());
+  const hour = theTime.getHours();
+  const isAm = hour < 12 || hour === 0;
+  let amPm = isAm ? 'AM' : 'PM';
 
-  // Hours
-let hour = dateObj.getHours();
-if (hour < 10) {
-  hour =  y + hour;
- }
- if (hour > 12) {
-   hour = hour - 12;
- }
- 
+  // Current Time
+  const currentTime = `${formatHour(hour)}:${minutes}:${seconds} ${amPm}`;
 
-// Minutes
-let minutes = dateObj.getMinutes();
-if (minutes < 10) {
-  minutes =  y + minutes;
- }
+  const timeOne = document.querySelector('.timeOne');
 
- // Seconds
-let seconds = dateObj.getSeconds();
-if (seconds < 10) {
-  seconds =  y + seconds;
- }
-
- // Current Time
-const currentTime = `${hour}:${minutes}:${seconds}`;
-
-// AM PM Calculator
-let ampm = dateObj.getHours();
-
-if (ampm < 12) {
-  ampm = 'AM';
-} else {
-  ampm = 'PM'
-};
-
-
-// Day
-// let weekDay = dateObj.toLocaleString("Default", { weekday: "long"});
-let weekDay = dateObj.getDay();
-
-const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-let day = weekDays[weekDay];
-
-
-// Month
-// let month = dateObj.toLocaleString("Default", {month: "long"});
-let dayOfMonth = dateObj.getMonth();
-
-const months = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-let month = months[dayOfMonth];
-
-// Numerical Day
-let numericalDate = dateObj.getDate();
-
-
-
-if(numericalDate == 1 || numericalDate == 21 || numericalDate == 31) {
-   numericalDate = numericalDate + "st";
-} else if (numericalDate == 2 || numericalDate == 22 ) {
-   numericalDate = numericalDate + "nd";
-}  else if (numericalDate == 3 || numericalDate == 23) {
-  numericalDate = numericalDate + "rd";
-} else  {
-  numericalDate = numericalDate + "th";
+  timeOne.textContent = currentTime;
 }
 
-
-// Year
-let currentYear = dateObj.getFullYear();
-
-
-
-// Adding to page
-let timeOne = document.querySelector('.timeOne');
-let timeTwo = document.querySelector('.timeTwo')
-
-timeOne.textContent = `${hour}:${minutes}:${seconds} ${ampm}`;
-
-timeTwo.textContent = `${day}, ${month} ${numericalDate} ${currentYear}`
-
+function addLeadingZero(number) {
+  return number < 10 ? '0' + number : number;
 }
 
-setInterval(showTimeAndDate, 1000);
+function formatHour(hour) {
+  hour = hour >= 13 ? hour - 12 : hour;
 
+  hour = hour === 0 ? hour + 12 : hour;
+  return hour;
+}
 
+function showDate() {
+  const theDate = new Date();
+  const day = days[theDate.getDay()];
+  const month = months[theDate.getMonth()];
+  const date = theDate.getDate();
+  const year = theDate.getFullYear();
 
+  const timeTwo = document.querySelector('.timeTwo');
+  const currentDate = `${day}, ${month} ${formatDateSuffix(date)} ${year}`;
 
+  timeTwo.textContent = currentDate;
+}
 
+function formatDateSuffix(date) {
+  if (date < 10 || date > 20) {
+    switch (date % 10) {
+      case 1:
+        return `${date}st`;
+      case 2:
+        return `${date}nd`;
+      case 3:
+        return `${date}rd`;
+    }
+  }
+  return `${date}th`;
+}
 
+const days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
+const months = [
+  'January',
+  'Febuary',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
 
+showTime();
+showDate();
+setInterval(() => {
+  showTime();
+  showDate();
+}, 1000);
